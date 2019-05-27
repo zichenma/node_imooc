6c47aa30d6f51596fba8ec188926b221591db94d
Liepin-NorthAmerica <nini10171017>
2019-05-27 10:27:14 -0400

    finished 6_6

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const queryString = require('querystring');
 const handleBlogRouter = require ('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
 
+// session 数据
+const SESSION_DATA = {};
+
+// 设置一天的过期时间
+const getCookieExpires = () => {
+    const d = new Date();
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
+    console.log('GMT', d.toGMTString());
+    return d.toGMTString();
+}
 
 // 用于处理 postdata
 const getPostData = req => {
@@ -58,7 +68,27 @@ const serverHandle = (req, res) => {
         const val = arr[1].trim();
         req.cookie[key] = val;
     })
-    console.log('req.cookie is', req.cookie);
+
+    // 解析 session
+    let userId = req.cookie.userid;
+    let needSetCookie = false;
+    if (userId) {
+        // 没有session
+        if (!SESSION_DATA[userId]) {
+            // 初始化session
+            SESSION_DATA[userId] = {};
+        } 
+        // 有session
+        req.session = SESSION_DATA[userId];
+    } else {
+        needSetCookie = true;
+        // 初始化userId
+        userId = `${Date.now()}_${Math.random()}`;
+        // 初始化session
+        SESSION_DATA[userId] = {};
+    }
+    req.session = SESSION_DATA[userId];
+
 
     // 处理 Post data
     getPostData(req).then(postData => {
@@ -78,6 +108,19 @@ const serverHandle = (req, res) => {
     
     if (blogResult) {
         blogResult.then(blogData => {
+            if (needSetCookie) {
+                // 操作 cookie 
+                // path 必须设置成根目录，否则默认的path为path=/api/user/login
+                // 如果与默认path不一致，cookie就会失效, 因此必须保证所有的网页验证登陆都有效
+                // httpOnly 禁止前端跟改cookie, 添加后前端document.cookie不会显示cookied,且不能修改
+                // 这里不要转行，转行会报错，暂时不知道原因
+                res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`);
+                // response header will show: 
+                // Set-Cookie: username=zhangsan; path=/
+                // 前端 console.log 会显示：
+                // document.cookie
+                // "username=zhangsan"
+            }
             res.end(
                JSON.stringify(blogData)
            )
@@ -97,6 +140,9 @@ const serverHandle = (req, res) => {
     const userResult = handleUserRouter(req, res);
     if (userResult) {
         userResult.then(userData => {
+            if (needSetCookie) {
+                res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`);
+            }
             res.end(
                 JSON.stringify(userData)
             );
--- src/router/user.js
+++ src/router/user.js
@@ -2,14 +2,6 @@ const { login } = require('../controller/user');
 const { SuccessModel, ErrorModel } = require('../model/resModel');
 
 
-// 设置一天的过期时间
-const getCookieExpires = () => {
-    const d = new Date();
-    d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
-    console.log('GMT', d.toGMTString());
-    return d.toGMTString();
-}
-
 const handleUserRouter = (req, res) => {
     const method = req.method; // GET POST
     // 登陆
@@ -20,17 +12,12 @@ const handleUserRouter = (req, res) => {
        const result = login(username, password);
        return result.then(data => {
            if (data.username) {
-                // 操作 cookie 
-                // path 必须设置成根目录，否则默认的path为path=/api/user/login
-                // 如果与默认path不一致，cookie就会失效, 因此必须保证所有的网页验证登陆都有效
-                // httpOnly 禁止前端跟改cookie, 添加后前端document.cookie不会显示cookied,且不能修改
-                // 这里不要转行，转行会报错，暂时不知道原因
-                res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`);
-                // response header will show: 
-                // Set-Cookie: username=zhangsan; path=/
-                // 前端 console.log 会显示：
-                // document.cookie
-                // "username=zhangsan"
+                // Cookie操作参考6—5，此处的comment
+                // 设置 session 
+                req.session.username = data.username;
+                req.session.realname = data.realname;
+
+                console.log('req.session', req.session);
                 return new SuccessModel();
            }
            return new ErrorModel('Login Failed');
@@ -39,9 +26,9 @@ const handleUserRouter = (req, res) => {
 
     // 登陆验证的测试
     if (method === 'GET' && req.path === '/api/user/login-test') {
-        if (req.cookie.username) {
+        if (req.session.username) {
             return Promise.resolve(new SuccessModel({
-                username: req.cookie.username
+                session: req.session
             }));
         }
         return Promise.resolve(new ErrorModel('Not login yet'));
