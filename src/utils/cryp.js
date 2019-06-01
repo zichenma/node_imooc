d61c5957d03403f63ecee1e55b4f55bea5ba0132
Liepin-NorthAmerica <nini10171017>
2019-06-01 15:26:31 -0400

    finsihed 8_3

---
+++ src/utils/cryp.js
@@ -0,0 +1,25 @@
+const crypto = require('crypto');
+// Secret Key
+// 必须保密
+const SECRET_KEY = 'WJiol_8776#';
+
+
+// md5 加密
+
+function md5(content) {
+    let md5 = crypto.createHash('md5');
+    return md5.update(content).digest('hex');
+}
+
+// 加密函数
+function genPassword(password) {
+    const str = `password=${password}&key=${SECRET_KEY}`;
+    return md5(str);
+}
+
+const res = genPassword('123');
+console.log(res);
+
+module.exports = {
+    genPassword
+}
