140e8d28e26e51fbd9ba35c3d39c51735bc58541
Liepin-NorthAmerica <nini10171017>
2019-06-02 16:30:02 -0400

    finsihed 9_7

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
 // 2. 引用路由：（在 /routes 里）
 const blogRouter = require('./routes/blog');
 const userRouter = require('./routes/user');
@@ -13,8 +14,8 @@ const userRouter = require('./routes/user');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 // 类似于 getPostData
@@ -24,8 +25,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
 // 3. 注册路由 （父路由）
 // 如果需要修改父路由，则只需要在此修改一次而已
 app.use('/api/blog', blogRouter);
--- routes/blog.js
+++ routes/blog.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog');
+const { SuccessModel, ErrorModel } = require('../model/resModel');
 
 // 1.设置 （子路由）
 router.get('/list', function(req, res, next) {
-    // 相当于：  JSON.stringify(resData) 
-    // 并且自动加上了： res.setHeader('Content-type', 'application/json')
-  res.json({
-      errno: 0,
-      data: [1, 2, 3]
-  })
+    let author = req.query.author || ''
+        const keyword = req.query.keyword || ''
+
+        // if (req.query.isadmin) {
+        //     // 管理员界面
+        //     const loginCheckResult = loginCheck(req)
+        //     if (loginCheckResult) {
+        //         // 未登录
+        //         return loginCheckResult
+        //     }
+        //     // 强制查询自己的博客
+        //     author = req.session.username
+        // }
+
+        const result = getList(author, keyword)
+        return result.then(listData => {
+            res.json(
+                new SuccessModel(listData)
+            )
+        })
 });
 
 router.get('/detail', function(req, res, next) {
