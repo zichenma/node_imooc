5aeca222b33859dc7341a7be6a70d5c922064d6f
Liepin-NorthAmerica <nini10171017>
2019-06-03 23:31:06 -0400

    finsihed 9_10

--- routes/blog.js
+++ routes/blog.js
@@ -14,16 +14,16 @@ router.get('/list', function(req, res, next) {
     let author = req.query.author || ''
         const keyword = req.query.keyword || ''
 
-        // if (req.query.isadmin) {
-        //     // 管理员界面
-        //     const loginCheckResult = loginCheck(req)
-        //     if (loginCheckResult) {
-        //         // 未登录
-        //         return loginCheckResult
-        //     }
-        //     // 强制查询自己的博客
-        //     author = req.session.username
-        // }
+        if (req.query.isadmin) {
+            // 管理员界面
+            // const loginCheckResult = loginCheck(req)
+            // if (loginCheckResult) {
+            //     // 未登录
+            //     return loginCheckResult
+            // }
+            // // 强制查询自己的博客
+            author = req.session.username
+        }
 
         const result = getList(author, keyword)
         return result.then(listData => {
